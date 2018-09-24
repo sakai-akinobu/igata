5b1916e525395557e1536447411b1435337d662e
sakai-akinobu <sakai.akinobu@gmail.com>
2018-09-24 11:42:38 +0900

    Fix target browsers.

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,7 @@ module.exports = {
   presets: [
     ['@babel/preset-env', {
       targets: {
-        edge: '17',
-        firefox: '60',
-        chrome: '67',
-        safari: '11.1',
+        'browsers': '> 0.25%, not dead',
       },
       useBuiltIns: 'usage',
     }],
