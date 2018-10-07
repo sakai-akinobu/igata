cbd66cddf1d25424df3570e916b719ee5982049e
sakai-akinobu <sakai.akinobu@gmail.com>
2018-10-07 20:58:19 +0900

    Add webpack.config.js file.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'lib'),
+    library: 'Igata',
+    libraryTarget: 'umd',
+  },
+};
